24c92040d804d5605d1a4dfb27e7a1b0c8a9871b
Alekya Gadiraju <gadirajualekya@gmail.com>
2021-04-04 15:23:58 -0400

    filtered data

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 
+// function buildMetadata(sample) {
+
 // Read in samples.json
 
-function buildMetadata(sample) {
-    d3.json("samples.json").then((data) => {
-      var metadata = data.metadata;
-      metadata.html("");
+    // d3.json("samples.json").then((data) => {
+    //   var metadata = data.metadata;
+    //   metadata.html("");
 
+// Filter data for sample number
+var Array = metadata.filter(sampleObj => sampleObj.id == sample);
+var data = Array[0];
+
+// Add data to panel
+Object.entries(data).forEach(([key, value]) => {
+    metadata.append("h6").text(`${key.toUpperCase()}: ${value}`);
+  });
+
+
+function buildCharts(sample) {
+    d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var Array = samples.filter(sampleObj => sampleObj.id == sample);
+    var data = Array[0];
+  
+    var otu_ids = data.otu_ids;
+    var otu_labels = data.otu_labels;
+    var sample_values = data.sample_values;
+  
+// Initialize the dashboard
+init();
